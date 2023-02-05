2daba940ca67e0ee694b7d6799a8b1b3c6f4c6e7
asfand687 <asfandyar687@gmail.com>
2023-02-05 23:56:46 +0500

    changed the button type to submit

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -64,8 +64,6 @@ const CreatePost = () => {
         })
 
         const data = await response.json()
-        console.log("RESPONSE", data)
-
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` })
       } catch (error) {
         console.log(error.message)
@@ -143,6 +141,7 @@ const CreatePost = () => {
         <div className="mt-10">
           <p className="mt-2 text-[#666e75] text-[14px]">Once you have created the image you want, you can share it with others in the community</p>
           <button
+            type="submit"
             className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
             Share With the Community
